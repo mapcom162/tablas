46703460353bbdd5685ecda2a02648cb44225a9c
mapcom162 <domelinve@outlook.es>
2026-07-15 14:19:34 -0500

    Su mejor version

--- multiplicar.js
+++ multiplicar.js
@@ -1,10 +1,17 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedor");
     let contenido = "";
-    let tabla = 5 ;
+    let tabla = parseInt(document.getElementById("txtTabla").value);
+    let titulo = document.getElementById("titulo")
+
+    if (isNaN(tabla)){
+        tabla = 0;
+    }
+
+    titulo.innerHTML = "Tabla del " + tabla
 
     for (let i = 1; i <= 10; i++){
         contenido = contenido + "<div class='tarjeta-operacion' >" + "<div class='multiplicacion'>" + tabla + "x" + i +"</div>" + "<div class='resultado'>" + tabla*i + "</div>" + "</div>"
-        contenedor.innerHTML = contenido;
     }
+    contenedor.innerHTML = contenido;
 }
